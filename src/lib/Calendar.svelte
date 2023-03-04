cfc6c08d84a1b8e43ab83ff2e7ce857d360a22f0
Jason Fritsche <fritsche.jason@gmail.com>
2023-03-04 12:09:56 -0600

    feat: fetch show data

---
+++ src/lib/Calendar.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { onMount } from "svelte";
+
+  let showData;
+
+  onMount(async () => {
+    await fetch(`https://raw.githubusercontent.com/JasonFritsche/showlist-austin-scraper/main/showdata.json`)
+      .then(r => r.json())
+      .then(data => {
+        console.log('data',data)
+        showData = data;
+      });
+  })
+
+</script>
+
+<h1>Calendar</h1>
